c01d603eafb85a0ce273b7a0ca8bff051f015f99
casey829 <caseymwangi31@gmail.com>
2022-06-02 11:20:17 +0300

    imort Business.css

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './Business.css';
 
 
 const Business = {
@@ -15,18 +16,18 @@ const Business = {
 
 class Business extends React.Component{
     render() {
-    return  <div className="Business">
+    return  <div className={this.Business}>
     <div className="image-container">
       <img src='https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg' alt=''/>
     </div>
     <h2>MarginOtto Pizzeria</h2>
-    <div className="Business-information">
-      <div className="Business-address">
+    <div className={this.Business-information}>
+      <div className={this.Business-address}>
         <p>1010 Paddington Way</p>
         <p>Bordertown</p>
         <p>NY 10101</p>
       </div>
-      <div className="Business-reviews">
+      <div className={Business.thisBusiness-reviews}>
         <h3>ITALIAN</h3>
         <h3 className="rating">4.5 stars</h3>
         <p>90 reviews</p>
